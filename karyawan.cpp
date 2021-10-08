c35d64438387b8f3326c0538858ccad80b4deac8
robbyyoni28 <robbyfebrianursaleh@gmail.com>
2021-10-08 09:57:43 -0700

    rubah variable

--- karyawan.cpp
+++ karyawan.cpp
@@ -6,12 +6,14 @@ using namespace std;
 
 int WinMain()
 {
-    cout << "**  welcome to the database **" << endl;
-    char choice;
-    string bookname;
-    string removebook;
+    cout << "=========================================" << endl;
+    cout << "============MANAJEMEN KARYAWAN==========="<< endl;
+    cout << "========================================="<< endl;
+    char pilihan;
+    string divisi;
+    string hapus_data;
     int countnum;
-    string authorname;
+    string nama_karyawan;
     string fullbook;
     vector<string> bookarray;
 
@@ -22,24 +24,24 @@ int WinMain()
         cout << " d. show number of books" << endl;
         cout << " e. show all of books" << endl;
         cout << "f . quit" << endl;
-        cin >> choice;
-        if(choice == 'f' ) {
+        cin >> pilihan;
+        if(pilihan == 'f' ) {
             printf("Terminating the program...");
         }
         else{
-            if(choice == 'a'){
+            if(pilihan == 'a'){
                 cout << "please enter name of book: " ;
-                cin >> bookname;
+                cin >> divisi;
                 cout << "please enter author name :";
-                cin >> authorname;
-                fullbook = bookname + " " + authorname;
+                cin >> nama_karyawan;
+                fullbook = divisi + " " + nama_karyawan;
                 bookarray.push_back(fullbook);
                 cout << "Book has been added" << endl;
-            }else if(choice =='b'){
+            }else if(pilihan =='b'){
                 cout << "Please enter book number to delete: " ;
-                cin >> removebook;
+                cin >> hapus_data;
                 for(int i = 0; i < bookarray.size() ; i++){
-                    if (bookarray[i].find(removebook) != std::string::npos) {
+                    if (bookarray[i].find(hapus_data) != std::string::npos) {
                         bookarray.erase (bookarray.begin()+i);
                         break;
                     }
@@ -47,14 +49,14 @@ int WinMain()
                 cout << " " << endl;
                 cout << "book has been removed" << endl;
                 cout << " " << endl;
-            }else if(choice == 'c'){
+            }else if(pilihan == 'c'){
                 bookarray.clear();
                 cout << "Database cleared"  << endl;
                 cout << " " << endl;
-            }else if(choice == 'd'){
+            }else if(pilihan == 'd'){
                 countnum = bookarray.size();
                 cout << "number of books in array:" << countnum << endl;
-            }else if(choice == 'e'){
+            }else if(pilihan == 'e'){
                 cout << "name of books in array " << endl;
                 for(int i = 0; i < bookarray.size() ; i++){
                     cout << i << " " << bookarray[i] <<  " " << endl; ;
@@ -63,7 +65,7 @@ int WinMain()
                 cout << " " << endl;
             }
         }
-    }while(choice != 'f');
+    }while(pilihan != 'f');
 
     return 0;
 }
