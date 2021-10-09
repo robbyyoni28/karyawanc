719c078568c62968c98a06165750ab9affcfb2d1
robbyyoni28 <robbyfebrianursaleh@gmail.com>
2021-10-09 10:02:03 +0800

    push

--- karyawan.cpp
+++ karyawan.cpp
@@ -14,52 +14,58 @@ int WinMain()
     string hapus_data;
     int countnum;
     string nama_karyawan;
-    string fullbook;
-    vector<string> bookarray;
+    string nohp;
+    char jenkel;
+    string semua_data;
+    vector<string> arraykaryawan;
 
     do{
         cout << " a. Tambah Data karyawan " << endl;
         cout << " b. Hapus Data Karyawan" << endl;
         cout << " c. Destroy Database" << endl;
-        cout << " d. Tampilkan id Karyawan" << endl;
+        cout << " d. Tampilkan jumlah Karyawan" << endl;
         cout << " e. Tampilkan semua Data Karyawan" << endl;
-        cout << "f . quit" << endl;
+        cout << "f . Exit" << endl;
         cin >> pilihan;
         if(pilihan == 'f' ) {
             printf("Terminating the program...");
         }
         else{
             if(pilihan == 'a'){
-                cout << "masukkan divisi penempatan karyawan: " ;
+                cout << "masukkan divisi penempatan karyawan : " ;
                 cin >> divisi;
                 cout << "masukkan nama karyawan :";
                 cin >> nama_karyawan;
-                fullbook = divisi + " " + nama_karyawan;
-                bookarray.push_back(fullbook);
-                cout << "Book has been added" << endl;
+                cout << "masukkan nomor hp karyawan :";
+                cin >> nohp;
+                cout << "jenis kelamin L/P  :" ;
+                cin >> jenkel;
+                semua_data = divisi + " " + nama_karyawan + " " + nohp + " " + jenkel;
+                arraykaryawan.push_back(semua_data);
+                cout << "data karyawan berhasil ditambahkan" << endl;
             }else if(pilihan =='b'){
-                cout << "Please enter book number to delete: " ;
+                cout << "pilih nomor karyawan yang akan dihapus: " ;
                 cin >> hapus_data;
-                for(int i = 0; i < bookarray.size() ; i++){
-                    if (bookarray[i].find(hapus_data) != std::string::npos) {
-                        bookarray.erase (bookarray.begin()+i);
+                for(int i = 0; i < arraykaryawan.size() ; i++){
+                    if (arraykaryawan[i].find(hapus_data) != std::string::npos) {
+                        arraykaryawan.erase (arraykaryawan.begin()+i);
                         break;
                     }
                 }
                 cout << " " << endl;
-                cout << "book has been removed" << endl;
+                cout << "data karyawan telah dihapus" << endl;
                 cout << " " << endl;
             }else if(pilihan == 'c'){
-                bookarray.clear();
+                arraykaryawan.clear();
                 cout << "Database cleared"  << endl;
                 cout << " " << endl;
             }else if(pilihan == 'd'){
-                countnum = bookarray.size();
-                cout << "number of books in array:" << countnum << endl;
+                countnum = arraykaryawan.size();
+                cout << "jumlah karyawan yang terdata disistem:" << countnum << endl;
             }else if(pilihan == 'e'){
-                cout << "name of books in array " << endl;
-                for(int i = 0; i < bookarray.size() ; i++){
-                    cout << i << " " << bookarray[i] <<  " " << endl; ;
+                cout << "data karyawan " << endl;
+                for(int i = 0; i < arraykaryawan.size() ; i++){
+                    cout << i << " " << arraykaryawan[i] <<  " " << endl; ;
 
                 }
                 cout << " " << endl;
